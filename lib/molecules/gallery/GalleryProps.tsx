57c44d2ba51049bfa88f510ad3567dcfad8067bf
Philippe Beck <philippebeck.dev@gmail.com>
2024-12-05 10:40:58 +0100

    Remove managing of button gallery

--- lib/molecules/gallery/GalleryProps.tsx
+++ lib/molecules/gallery/GalleryProps.tsx
@@ -2,25 +2,22 @@
  * ! GALLERY ITEM PROPS INTERFACE
  * @interface GalleryItemProps
  * @description The Properties of the Gallery Item
- * 
+ *
  * @property {string} url
  *  The URL of the Item
- * 
- * @property {string} name
- *  The Name of the Item
- * 
+ *
  * @property {string} detail
  *  The Detail of the Item
- * 
+ *
  * @property {Object} caption
  *  The Caption of the Item
- * 
+ *
  * @property {string} caption.title
  *  The Title of the Item Caption
- * 
+ *
  * @property {Array} caption.technos
  *  The Technologies of the Item Caption
- * 
+ *
  * @property {Object} image
  *  The Image of the Item
  *
@@ -32,13 +29,12 @@
  */
 export interface GalleryItemProps {
   url: string
-  name?: string
-  detail?: string
-  caption?: {
+  detail: string
+  caption: {
     title: string
     technos?: string[]
   };
-  image?: {
+  image: {
     url: string
     alt: string
   }
