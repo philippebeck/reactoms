8c871697fe0ee6c49508d9c47878d857def66f13
Philippe Beck <philippebeck.dev@gmail.com>
2024-12-06 10:11:16 +0100

    Add props id & className to Footer

--- lib/molecules/footer/FooterProps.tsx
+++ lib/molecules/footer/FooterProps.tsx
@@ -5,9 +5,13 @@ import { ButtonProps } from '../button/ButtonProps'
  * @interface FooterProps
  * @description The Properties of the Footer
  *
+ * @property {string} id
+ *  The Id of the Footer
+ * 
  * @property {Array<ButtonProps>} links
  *  The Links Array of the Footer
  */
 export interface FooterProps {
+  id: string
   links: Array<ButtonProps>
 }
